87d5282a7b3ae401b1f94a6dcd6bebdd238cfdc4
Joe Biellik <joebiellik@gmail.com>
2018-12-29 17:26:21 +0000

    Use SDK version constant

--- Chat.Client/Properties/AssemblyInfo.cs
+++ Chat.Client/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using NFive.SDK.Client;
 
-[assembly: ClientPlugin(1)]
+[assembly: ClientPlugin(SDK.Version)]
 
 [assembly: AssemblyTitle("NFive Chat")]
 [assembly: AssemblyDescription("Chat")]
--- Chat.Server/Properties/AssemblyInfo.cs
+++ Chat.Server/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using NFive.SDK.Server;
 
-[assembly: ServerPlugin(1)]
+[assembly: ServerPlugin(SDK.Version)]
 
 [assembly: AssemblyTitle("NFive Chat")]
 [assembly: AssemblyDescription("Chat")]
